dc1502687bd26015ae6362f9f7959a89552f3aa4
IvyTheIV <larry748714@gmail.com>
2022-01-20 19:23:43 +0600

    remove old functions

--- src/controller.cpp
+++ src/controller.cpp
@@ -3,72 +3,10 @@
 class Controller
 {
 private:
-    std::vector<std::string> GetStringValues(std::string str, char delimiter)
+
+    bool IsStringANumber(string& str)
     {
-        int occurence = 0;
-        std::string word = "";
-        std::vector<std::string> words;
-        for (int i = 0; i < str.length(); i++)
-        {
-            if (str[i] == delimiter)
-            {
-                occurence++;
-
-                if (occurence % 2 == 0)
-                {
-                    words.push_back(word);
-                    word = "";
-                }
-
-                continue;
-            }
-
-            if (occurence % 2 != 0)
-            {
-                word += str[i];
-            }
-        }
-        return words;
-    }
-
-    std::vector<std::string> GetStringCommands(std::string str, char delimiter)
-    {
-        int occurence = 0;
-        std::string word = "";
-        std::vector<std::string> words;
-        for (int i = 0; i < str.length(); i++)
-        {
-            if (str[i] == delimiter)
-            {
-                occurence++;
-
-                if (occurence % 2 != 0)
-                {
-                    words.push_back(word);
-                    word = "";
-                }
-
-                continue;
-            }
-
-            if (occurence % 2 == 0)
-            {
-                word += str[i];
-            }
-        }
-        return words;
-    }
-
-    bool VectorContains(std::vector<std::string> vector, std::string match)
-    {
-        for (int i = 0; i < vector.size(); i++)
-        {
-            if (vector[i].find(match) != std::string::npos)
-            {
-                return true;
-            }
-        }
-        return false;
+        return std::all_of(str.begin(), str.end(), ::isdigit); //fancy c++11 code
     }
 
 public:
@@ -91,145 +29,8 @@ public:
         return hashtables.size();
     }
 
-    /*Function to interpret the input string. Spilts the string into commands and words,
+    /*Function to interpret the input string. Spilts the string into words,
     then checks which commands to execute.*/
-    std::string ResolveStringCommand(std::string command)
-    {
-        std::vector<std::string> words = GetStringValues(command, '/');
-        std::vector<std::string> commands = GetStringCommands(command, '/');
-
-        //TODO: Check for vulnerabilities. Maybe clean the code up a bit...
-        if (VectorContains(commands, "AUTH"))
-        {
-            if (protectedByKey)
-            {
-                if (words.size() > 0 && words[0] != "")
-                {
-                    if (hashtables[0].Contains(words[0]))
-                    {
-                        if (!hashtables[0].Contains(tempIP))
-                        {
-                            hashtables[0].Add(tempIP);
-                        }
-                        return GetHttpStatusCode(200);
-                    }
-                }
-                return GetHttpStatusCode(403);
-            }
-            return GetHttpStatusCode(403);
-        }
-
-        else if (protectedByKey == false || hashtables[0].Contains(tempIP))
-        {
-            if (VectorContains(commands, "INSERT"))
-            {
-                if (VectorContains(commands, "INTO") && words[1] != "")
-                {
-                    int index = atoi(words[1].c_str());
-                    if (index < 1)
-                        return GetHttpStatusCode(403);
-                    if (VectorContains(commands, "WHEREKEY") && words.size() >= 3)
-                    {
-                        int ttl = 0;
-                        if (VectorContains(commands, "TTL"))
-                        {
-                            ttl = atoi(words[3].c_str());
-                        }
-
-                        if (index >= hashtables.size())
-                        {
-                            hashtables.resize(index + 1);
-                        }
-
-                        if (index < hashtables.size())
-                        {
-                            int hash = hashtables[index].Add(words[2], words[0]);
-                            if (ttl > 0)
-                            {
-                                Packet p;
-                                p.hash = hash;
-                                p.time_to_live = ttl;
-                                p.table = index;
-                                packets.push_back(p);
-                            }
-                            return GetHttpStatusCode(201);
-                        }
-                    }
-                    else if (!VectorContains(commands, "WHEREKEY"))
-                    {
-                        if (index < hashtables.size())
-                        {
-                            hashtables[index].Add(words[0]);
-                            return GetHttpStatusCode(201);
-                        }
-                    }
-                }
-            }
-
-            else if (VectorContains(commands, "DELETE"))
-            {
-                if (VectorContains(commands, "FROM") && words.size() >= 2 && words[1] != "")
-                {
-                    int index = atoi(words[1].c_str());
-                    if (index < 1)
-                        return GetHttpStatusCode(403);
-
-                    if (index < hashtables.size())
-                    {
-                        if (VectorContains(commands, "ASINT"))
-                        {
-                            hashtables[index].Remove(atoi(words[0].c_str()));
-                            return GetHttpStatusCode(200);
-                        }
-                        else if (!VectorContains(commands, "ASINT"))
-                        {
-                            hashtables[index].Remove(words[0]);
-                            return GetHttpStatusCode(200);
-                        }
-                    }
-                }
-            }
-
-            /* TODO: Currently if the db is of {x} size and you try to fetch
-               from the {x} table, it will throw a segmentation fault.*/
-            else if (VectorContains(commands, "FETCH"))
-            {
-                if (VectorContains(commands, "FROM") && words.size() >= 2 && words[1] != "")
-                {
-                    int index = atoi(words[1].c_str());
-                    if (index < 1)
-                        return GetHttpStatusCode(403);
-
-                    std::string result;
-                    if (index < hashtables.size())
-                    {
-                        if (words[0].length() == 1 && words[0] == "*")
-                        {
-                            result = hashtables[index].GetAll();
-                        }
-                        else
-                        {
-                            result = hashtables[index].Get(words[0]);
-                        }
-                        if (result == "")
-                        {
-                            return GetHttpStatusCode(404);
-                        }
-                        return result;
-                    }
-                }
-            }
-            return GetHttpStatusCode(404);
-        }
-        return GetHttpStatusCode(401);
-    }
-
-
-    bool IsStringANumber(string& str)
-    {
-        return std::all_of(str.begin(), str.end(), ::isdigit); //fancy c++11 code
-    }
-
     string GetResolvedResponse(string request)
     {
         request += " "; // required for the check below
@@ -245,9 +46,6 @@ public:
         }
         words.back().pop_back();
         words.back().pop_back();
-        /*Adding empty strings to the vector to prevent segmentation faults.
-        This is in case the user didn't provide enough parameters for the command.*/
-        //words.resize(words.size() + 8);
 
         if (words[0].find("AUTH") != string::npos && words.size() > 1)
         {
@@ -315,14 +113,15 @@ public:
         {
             string& key = words[1];
             words[1].pop_back();
-
             int table = atoi(words[2].c_str());
             if (table <= 0) return GetHttpStatusCode(403);
+
             if (IsStringANumber(key))
             {
                 hashtables[table].Remove(atoi(key.c_str()));
                 return GetHttpStatusCode(200);
             }
+
             hashtables[table].Remove(key);
             return GetHttpStatusCode(200);
         }
