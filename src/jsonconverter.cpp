5ed66628db5d3c0ac762f751570f9f56caee0c94
beyluta <beyluta1@gmail.com>
2022-04-11 14:00:48 +0200

    add initial files for json converter

---
+++ src/jsonconverter.cpp
@@ -0,0 +1,139 @@
+#include "jsonconverter.h"
+
+class JsonParser
+{
+public:
+    enum JsonType
+    {
+        JSON_OBJECT = 0,
+        JSON_ARRAY = 1,
+        JSON_STRING = 2,
+        JSON_NUMBER = 3,
+        JSON_TRUE = 4,
+        JSON_FALSE = 5,
+        JSON_NULL = 6
+    };
+
+    string Get(string json)
+    {
+        return json;
+    }
+
+    JsonType GetType(string json)
+    {
+        json.erase(std::remove_if(json.begin(), json.end(), ::isspace), json.end());
+        int end = json.length() - 1;
+
+        if (json[0] == '{' && json[end] == '}')
+        {
+            return JSON_OBJECT;
+        }
+        if (json[0] == '[' && json[end] == ']')
+        {
+            return JSON_ARRAY;
+        }
+
+        return JSON_NULL;
+    }
+};
+
+class Json
+{
+private:
+    vector<string> values;
+
+    int Hash(string key)
+    {
+        int hash = 0;
+        for (int i = 0; i < key.size(); i++)
+        {
+            hash += key[i];
+        }
+        return hash;
+    }
+
+    bool Contains(string key)
+    {
+        if (values.size() < Hash(key))
+        {
+            return false;
+        }
+
+        return values[Hash(key)].length() > 0 ? true : false;
+    }
+
+    void Add(string key, string value)
+    {
+        if (values.size() < Hash(key))
+        {
+            values.resize(Hash(key) + 1);
+        }
+
+        if (!Contains(key))
+        {
+            values[Hash(key)] = value;
+        }
+    }
+
+    string Get(string key)
+    {
+        return Contains(key) ? values[Hash(key)] : "";
+    }
+
+    vector<int> GetCharCount(string includedCharacters, string json)
+    {
+        vector<int> charIndexes;
+
+        for (int i = 0; i < json.length(); i++)
+        {
+            for (int j = 0; j < includedCharacters.length(); j++)
+            {
+                if (json[i] == includedCharacters[j])
+                {
+                    charIndexes.push_back(i);
+                }
+            }
+        }
+
+        return charIndexes;
+    }
+
+    // { "key":"this", "value":"that" }
+public:
+    Json(string json)
+    {
+        vector<int> charIndexes = GetCharCount("\"[]{}", json);
+
+        if (charIndexes.size() % 2 != 0)
+        {
+            cout << "Invalid JSON Object: " << json << endl;
+            return;
+        }
+
+        
+        
+        // for (int i = 0; i < charIndexes.size(); i++)
+        // {
+        //     if (i + 1 < charIndexes.size())
+        //     {
+        //         if (json[charIndexes[i]] == json[charIndexes[i + 1]])
+        //         {
+        //             cout << json[charIndexes[i]] << " and " << json[charIndexes[i + 1]] << endl;
+        //         }
+        //     }
+        // }
+    }
+
+    string operator[](string key)
+    {
+        return "";
+    }
+};
+
+int main()
+{
+    // JsonParser parser;
+    // cout << parser.GetType("[{ \"key\" : \"value\"}]") << "\n";
+    Json json("{ \"key\" : \"value\" }");
+    return 0;
+}
---
+++ src/jsonconverter.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+namespace JsonUtilities
+{
+    class JsonParser
+    {
+    public:
+        enum JsonType
+        {
+            JSON_OBJECT = 0,
+            JSON_ARRAY = 1,
+            JSON_STRING = 2,
+            JSON_NUMBER = 3,
+            JSON_TRUE = 4,
+            JSON_FALSE = 5,
+            JSON_NULL = 6
+        };
+        string Get(string json);
+        string GetType(string json);
+    };
+
+    class Json
+    {
+    private:
+        vector<string> values;
+        int Hash(string key);
+        bool Contains(string key);
+        void Add(string key, string value);
+        string Get(string key);
+        vector<int> GetCharCount(string includedCharacters, string json);
+
+    public:
+        Json(string json);
+        string operator[](string key);
+    };
+}
