2621fba5c32e40345234b57b2a9911f5a8fc6819
beyluta <beyluta1@gmail.com>
2024-05-11 15:40:54 +0200

    fix issue saving and loading tables

--- templates/main.js
+++ templates/main.js
@@ -17,7 +17,19 @@ class Program {
      * The current page
      * @type {number}
     */
-    page = 0;
+    page = -4;
+
+    /**
+     * The number of items per page
+     * @type {number}
+    */
+    items = 4;
+
+    /**
+     * The previous items
+     * @type {Array<any>}
+    */
+    pastItems = [];
 
 
     /**
@@ -26,6 +38,8 @@ class Program {
      */
     constructor() {
         this.database = new Database("localhost", 8080);
+        this.populateNext = this.populateNext.bind(this);
+        this.populatePrevious = this.populatePrevious.bind(this);
         this.populateNext();
     }
 
@@ -35,18 +49,56 @@ class Program {
      */
     async populateNext() {
         try {
-            const data = await this.database.queryByRange('ALL', 1, this.page, this.page + 4);
-            this.total = data[0].total;
-            this.page += 4;
+            this.pastItems?.map((record) => record.remove());
+            console.log("from", this.page, "to", this.page + this.items);
+            this.page += this.items;
+            const data = await this.database.queryByRange('ALL', 1, this.page, this.page + this.items);
+            this.total = Number(data[0].total);
+            
 
-            for (const record of data) {
-                const el = document.createElement('a');
-                el.setAttribute('class', 'collection-item');
-                el.innerText = JSON.stringify(record.value, null, '\t');
-                document.querySelector('.collection').appendChild(el);
-            }
+            this.pastItems = data.map((record) => {
+                const el = document.createElement('tr');
+                const id = document.createElement('td');
+                const value = document.createElement('td');
+                const total = document.createElement('td');
+                
+                id.innerText = JSON.stringify(record.index | "");
+                value.innerText = JSON.stringify(record.value, null, '\t');
+                total.innerText = JSON.stringify(record.total);
+                el.appendChild(id);
+                el.appendChild(value);
+                el.appendChild(total);
+                document.querySelector('#table').appendChild(el);
+                return el;
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-            console.log(data, this.total);
+    async populatePrevious() {
+        try {
+            this.pastItems?.map((record) => record.remove());
+            this.page -= this.items;
+            console.log("from", this.page, "to", this.page + this.items);
+            const data = await this.database.queryByRange('ALL', 1, this.page, this.page + this.items);
+            this.total = Number(data[0].total);
+
+            this.pastItems = data.map((record) => {
+                const el = document.createElement('tr');
+                const id = document.createElement('td');
+                const value = document.createElement('td');
+                const total = document.createElement('td');
+                
+                id.innerText = JSON.stringify(record.index | "");
+                value.innerText = JSON.stringify(record.value, null, '\t');
+                total.innerText = JSON.stringify(record.total);
+                el.appendChild(id);
+                el.appendChild(value);
+                el.appendChild(total);
+                document.querySelector('#table').appendChild(el);
+                return el;
+            });
         } catch (error) {
             console.error(error);
         }
@@ -63,3 +115,5 @@ class Program {
 
 const program = new Program();
 document.querySelector('#reload-btn').addEventListener('click', program.reload);
+document.querySelector('#next-btn').addEventListener('click', program.populateNext);
+document.querySelector('#prev-btn').addEventListener('click', program.populatePrevious);
