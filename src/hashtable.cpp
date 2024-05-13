457f215db75feb4bdcb5885859cfffbb5c497bd9
beyluta <beyluta1@gmail.com>
2024-05-13 15:44:23 +0200

    improve hashing function

--- src/hashtable.cpp
+++ src/hashtable.cpp
@@ -35,15 +35,16 @@ std::string Hashtable::GetSameIndexValues(LinkNode *node)
 
 int Hashtable::Hash(std::string value)
 {
-    int hash = 0;
+    unsigned long hash = 5381;
 
-    for (int i = 0; i < value.length(); i++)
+    for (int i = 0; i < value.size(); i++)
     {
-        hash += value[i];
+        hash = ((hash << 5) + hash) + value[i];
     }
 
     return hash % MAX_CAPACITY;
 }
+
 
 void Hashtable::AddTo(int hash, std::string value)
 {
